6a6e40d7d3c3ce28b224a061ef482d6bb92b45eb
Ricardo Cordeiro <Ricardo.Cordeiro@gft.com>
2019-10-02 22:11:07 +0100

    Transitions bootstrap deployment to terraform 0.12
    This requires the use of terraform 0.12 for bootstrap deployments from
    now on.

--- tb-gcp-tr/bootstrap/datasources.tf
+++ tb-gcp-tr/bootstrap/datasources.tf
@@ -12,19 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-data "template_file" "startup-script" {
-  template = "${file(var.metadata_startup_script)}"
-  vars = {
-    clusters_master_whitelist_ip = "${google_compute_address.nat_gw_ip.address}"
-    region = "${var.region}"
-    region_zone = "${var.region_zone}"
-    root_id = "${var.folder_id}"
-    root_is_org = "false"
-    billing_account_id = "${var.billing_account_id}"
-    tb_discriminator = "${var.tb_discriminator}"
-    terraform_state_bucket_name = "${google_storage_bucket.terraform-state-bucket-res.name}"
-  }
+data "google_client_config" "this" {
 }
 
-data "google_client_config" "this" {}
--- tb-gcp-tr/bootstrap/variables.tf
+++ tb-gcp-tr/bootstrap/variables.tf
@@ -14,94 +14,101 @@
 
 #PROVIDER
 variable "region" {
-    type = "string"
-    default = "europe-west2"
-    description = "Region name."
+  type        = string
+  default     = "europe-west2"
+  description = "Region name."
 }
 
 variable "region_zone" {
-    type = "string"
-    default = "europe-west2-a"
-    description = "Zone name in the region provided."
+  type        = string
+  default     = "europe-west2-a"
+  description = "Zone name in the region provided."
 }
 
 variable "folder_id" {
-    type = "string"
-    description = "Id for the parent folder where this project will be created."
+  type        = string
+  description = "Id for the parent folder where this project will be created."
 }
 
 variable "billing_account_id" {
-    type = "string"
-    description = "Id of billing account to which bootstrap project will be assigned"
+  type        = string
+  description = "Id of billing account to which bootstrap project will be assigned"
 }
 
 variable "tb_discriminator" {
-  type = "string"
+  type        = string
   description = "sufix added to the names/IDs such elements like Tranquility Base folder, Bootstrap project what allows their coexistence with other sibling TBase instances within the same Organisation/Folder. Example: 'uat', 'dev-am', ''. For the empty value no suffix will be added (thus production ready)."
 }
 
 #CREATE-CUSTOM-VPC
 variable "vpc_name" {
   default = "bootstrap"
-  type = "string"
+  type    = string
 }
 
 variable "subnet_name" {
   default = "bootstrap"
-  type = "string"
+  type    = string
 }
 
 variable "subnet_cidr_range" {
   default = "192.168.0.0/28"
-  type = "string"
+  type    = string
 }
 
 #CREATE-CLOUD-NAT-ROUTER
 variable "router_name" {
-  type = "string"
-  default = "vpc-network-router"
+  type        = string
+  default     = "vpc-network-router"
   description = "Cloud NAT router name"
 }
 
 variable "router_nat_name" {
-  type = "string"
-  default = "vpc-network-nat-gateway"
+  type        = string
+  default     = "vpc-network-nat-gateway"
   description = "Cloud NAT gateway name"
 }
 
 #CREATE-BOOTSTRAP-TERRAFORM-SERVER
 variable "terraform_server_name" {
-  type = "string"
+  type    = string
   default = "terraform-server"
 }
 
 variable "terraform_server_machine_type" {
-  type = "string"
+  type    = string
   default = "g1-small"
 }
 
 variable "bootstrap_host_disk_image" {
-  type = "string"
-  default = "family/tb-tr-debian-9"
+  type        = string
+  default     = "family/tb-tr-debian-9"
   description = "reference to a GCR VM image which will be used for terraform-server instance creation. For more details regarding accepted image references see: https://www.terraform.io/docs/providers/google/r/compute_instance.html#image"
 }
 
 variable "metadata_startup_script" {
-  type = "string"
-  default = "files/bootstrap.sh"
+  type        = string
+  default     = "files/bootstrap.sh"
   description = "Local path to a bootstrap startup script template. Default value is 'files/bootstrap.sh'."
 }
 
 variable "scopes" {
-  type = "list"
-  default = ["https://www.googleapis.com/auth/cloud-platform"]
+  type        = list(string)
+  default     = ["https://www.googleapis.com/auth/cloud-platform"]
   description = "A list of service scopes attached to the bootstrap terraform server. To allow full access to all Cloud APIs, use the cloud-platform scope. For other scopes see here: https://cloud.google.com/sdk/gcloud/reference/alpha/compute/instances/set-scopes#--scopes "
 }
 
 variable "main_iam_service_account_roles" {
-  type = "list"
-  default = ["roles/resourcemanager.folderAdmin", "roles/resourcemanager.projectCreator",
-    "roles/resourcemanager.projectDeleter", "roles/billing.projectManager", "roles/compute.xpnAdmin", "roles/owner",
-    "roles/compute.networkAdmin"]
+  type = list(string)
+  default = [
+    "roles/resourcemanager.folderAdmin",
+    "roles/resourcemanager.projectCreator",
+    "roles/resourcemanager.projectDeleter",
+    "roles/billing.projectManager",
+    "roles/compute.xpnAdmin",
+    "roles/owner",
+    "roles/compute.networkAdmin",
+  ]
   description = "Roles attached to service account"
 }
+
