84b49d130086b2c156c66ce2a75a47a286b4e402
Ricardo Cordeiro <Ricardo.Cordeiro@gft.com>
2019-10-04 21:17:53 +0100

    itop/examples/simple TF 0.12 upgrade

--- tb-gcp-tr/itop/examples/simple/provider.tf
+++ tb-gcp-tr/itop/examples/simple/provider.tf
@@ -13,12 +13,12 @@
 # limitations under the License.
 
 provider "google" {
-  project = "${var.host_project_id}"
-  region = "${var.region}"
-  zone = "${var.region_zone}"
+  project = var.host_project_id
+  region  = var.region
+  zone    = var.region_zone
 }
-
 
 provider "kubernetes" {
   config_context_cluster = "gke_${var.host_project_id}_${var.region_zone}_${var.k8_cluster_name}"
 }
+
