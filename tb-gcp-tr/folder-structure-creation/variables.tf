c19bf9a1c92ee7e551c8573668513acc0a720821
Ricardo Cordeiro <Ricardo.Cordeiro@gft.com>
2019-10-04 15:18:41 +0100

    Upgrades folder-structure-creation to TF 0.12

--- tb-gcp-tr/folder-structure-creation/variables.tf
+++ tb-gcp-tr/folder-structure-creation/variables.tf
@@ -12,31 +12,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-variable "region" { 
-    type = "string"
-    default = "europe-west2"
-    description = "region name."
+variable "region" {
+  type        = string
+  default     = "europe-west2"
+  description = "region name."
 }
 
 variable "region_zone" {
-    type = "string"
-    default = "europe-west2-a"
-    description = "zone name in the region provided."
+  type        = string
+  default     = "europe-west2-a"
+  description = "zone name in the region provided."
 }
 
 variable "root_id" {
-    type = "string"
-    description = "id for the parent where these folders will be created."
+  type        = string
+  description = "id for the parent where these folders will be created."
 }
 
 variable "root_is_org" {
-    type = "string"
-    default = "true"
-    description = "determines whether root directory is an org or a directory inside an org."
+  type        = string
+  default     = "true"
+  description = "determines whether root directory is an org or a directory inside an org."
 }
 
 variable "tb_discriminator" {
-    type = "string"
-    default = ""
-    description = "sufix added to the Tranquility Base folder allowing coexistence of other TBase instances within the same Organisation/Folder. Example: 'uat', 'dev-am'. Default value is empty so no suffix will be added."
+  type        = string
+  default     = ""
+  description = "sufix added to the Tranquility Base folder allowing coexistence of other TBase instances within the same Organisation/Folder. Example: 'uat', 'dev-am'. Default value is empty so no suffix will be added."
 }
+
