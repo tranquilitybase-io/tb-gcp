4fd4784f3ee03a567a4623aaa48a4c9b282a5ea3
Ricardo Cordeiro <Ricardo.Cordeiro@gft.com>
2019-10-04 21:38:25 +0100

    http-load-balancer TF 0.12 upgrade

--- tb-gcp-tr/http-load-balancer/variables.tf
+++ tb-gcp-tr/http-load-balancer/variables.tf
@@ -19,17 +19,17 @@
 
 variable "project" {
   description = "The project ID to create the resources in."
-  type        = "string"
+  type        = string
 }
 
 variable "name" {
   description = "Name for the load balancer forwarding rule and prefix for supporting resources."
-  type        = "string"
+  type        = string
 }
 
 variable "url_map" {
   description = "A reference (self_link) to the url_map resource to use."
-  type        = "string"
+  type        = string
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -37,34 +37,33 @@ variable "url_map" {
 # These variables have defaults, but may be overridden by the operator.
 # ---------------------------------------------------------------------------------------------------------------------
 
-
 variable "ssl_certificates" {
   description = "List of SSL cert self links. Required if 'enable_ssl' is 'true'."
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "create_dns_entries" {
   description = "If set to true, create a DNS A Record in Cloud DNS for each domain specified in 'custom_domain_names'."
-  type        = "string"
+  type        = string
   default     = "false"
 }
 
 variable "custom_domain_names" {
   description = "List of custom domain names."
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "dns_managed_zone_name" {
   description = "The name of the Cloud DNS Managed Zone in which to create the DNS A Records specified in 'var.custom_domain_names'. Only used if 'var.create_dns_entries' is true."
-  type        = "string"
+  type        = string
   default     = "replace-me"
 }
 
 variable "dns_record_ttl" {
   description = "The time-to-live for the site A records (seconds)"
-  type        = "string"
+  type        = string
   default     = "300"
 }
 
