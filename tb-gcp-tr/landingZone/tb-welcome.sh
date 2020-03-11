785e6c58bf9071a71278d0c41c6cda8619f1152d
snul <simon.underhill@gft.com>
2020-03-11 14:38:43 +0000

    delete unnecessary files

---
+++ tb-gcp-tr/landingZone/tb-welcome.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+# Copyright 2019 The Tranquility Base Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+cd /opt/tb/repo/tb-gcp-tr/landingZone/no-itop
+#Retrieve terraform vars
+source input.auto.tfvars
+
+MAX_ATTEMPTS=10
+MAX_ATTEMPTS_INIT=3
+DELAY_BETWEEN_ATTEMPTS=60
+export HOME=/root
+
+#Retrieve project ID
+PROJECT_ID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
+#Initialise variables
+SA_NAME="bootstrap-tb-sa"
+SA_EMAIL=${SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com
+FOLDER_ID=${root_id}
+BILLING_ID=${billing_account_id}
+
+# The deployment will be done using the boostrap SA
+echo "Setting [${SA_EMAIL}] as active account"
+gcloud config set account ${SA_EMAIL}
+
+# Create TF state bucket
+gsutil mb "gs://${terraform_state_bucket_name}/"
+
+echo "Terraform init"
+# delay to allow nat gateway to be created
+sleep $DELAY_BETWEEN_ATTEMPTS
+apply_failures=0
+while [ $apply_failures -lt $MAX_ATTEMPTS_INIT ]; do
+terraform init -backend-config="bucket=${terraform_state_bucket_name}" -backend-config="prefix=landingZone"
+if [ $? -eq 0 ]; then
+echo "Terraform initialised successfully"
+break
+fi
+if [ $((apply_failures +1)) -eq $MAX_ATTEMPTS_INIT ]; then
+echo "Maximum of $MAX_ATTEMPTS_INIT reached. Moving on..."
+break
+fi
+echo "Terraform init failed."
+apply_failures=$(($apply_failures + 1))
+echo "Retry #$apply_failures starting in $DELAY_BETWEEN_ATTEMPTS seconds."
+sleep $DELAY_BETWEEN_ATTEMPTS
+done
+
+apply_failures=0
+while [ $apply_failures -lt $MAX_ATTEMPTS ]; do
+terraform apply -var-file input.tfvars -auto-approve
+if [ $? -eq 0 ]; then
+echo "Landing Zone successfully deployed."
+break
+fi
+if [ $((apply_failures +1)) -eq $MAX_ATTEMPTS ]; then
+echo "Maximum of $MAX_ATTEMPTS reached. Moving on..."
+break
+fi
+echo "Landing Zone deployment failed."
+apply_failures=$(($apply_failures + 1))
+echo "Retry #$apply_failures starting in $DELAY_BETWEEN_ATTEMPTS seconds."
+sleep $DELAY_BETWEEN_ATTEMPTS
+done
+
+# Commit current TB terraform code to GCR
+cd /tmp
+gcloud source repos create tb-terraform-code
+gcloud source repos clone tb-terraform-code
+cd /tmp/tb-terraform-code
+rsync -a /opt/tb/repo/ .
+git init
+git add .
+git commit -m "Landing zone terraform script"
+git push -u origin master
+
+
