aaf327486a382c7b738169521fd18de6350ce1b6
cmac199 <21153362@student.uwa.edu.au>
2020-04-16 22:21:20 +0100

    added initial files for auto deleter #127

---
+++ tb-gcp-tools/tb-auto-projects-deleter/app.py
@@ -0,0 +1,56 @@
+from google.cloud import resource_manager
+
+import subprocess
+import sys
+
+BUILD_PROJECT_ID = sys.argv[1]
+BILLING_ACCOUNT = sys.argv[2]
+PATH_DELETE_SCRIPT = sys.argv[3]
+PATH_SA_PERMISSION_SCRIPT = sys.argv[4]
+BUILD_SA_NAME = "{}@cloudbuild.gserviceaccount.com".format(BUILD_PROJECT_ID)
+BOOTSTRAP_PREFIX = "bootstrap-"
+NO_DELETE_LABEL = "no-delete"
+
+
+def main():
+    client = resource_manager.Client()
+
+    projects_to_delete_list = \
+        list(filter(filter_non_tb_projects, client.list_projects()))
+
+    for project in projects_to_delete_list:
+        folder_id = project.parent['id']
+        give_build_sa_permission_to_delete(project.id, BILLING_ACCOUNT, folder_id, BUILD_SA_NAME,
+                                           PATH_SA_PERMISSION_SCRIPT)
+        print("ATTEMPTING TO DELETE PROJECT {}".format(project.name))
+        # call_deleter_script_for_project(project.id, BILLING_ACCOUNT, folder_id, PATH_DELETE_SCRIPT)
+
+
+def filter_non_tb_projects(project):
+    if project.name.startswith(BOOTSTRAP_PREFIX) and NO_DELETE_LABEL in project.labels:
+        return True
+    else:
+        return False
+
+
+def give_build_sa_permission_to_delete(path_to_script, project_id, billing_account, sa_name, folder_id):
+    cmd = "bash {} -r {} -f {} -b {}".format(path_to_script, project_id, billing_account, sa_name, folder_id)
+    run_bash_command(cmd)
+    return
+
+
+def run_bash_command(cmd):
+    ps = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output = ps.communicate()[0].decode()
+    print(output)
+
+
+def call_deleter_script_for_project(project, billing_account, folder_id, path_to_script):
+    print("DELETING RESOURCES CREATED BY " + project.name + " PROJECT")
+    random_id = get_random_id_from_project(project)
+    cmd = "yes Y | bash {} -r {} -f {} -b {}".format(path_to_script, random_id, folder_id, billing_account)
+    run_bash_command(cmd)
+
+
+def get_random_id_from_project(project):
+    return project.name[-8:]
---
+++ tb-gcp-tools/tb-auto-projects-deleter/service_account_permissions.sh
@@ -0,0 +1,28 @@
+args=("$@")
+
+PROJECT_ID=${args[0]}
+BILLING_ID=${args[1]}
+SA_EMAIL=${args[2]}
+FOLDER_ID=${args[3]}
+
+#Add permissions at the project level
+echo "Adding permissions at the project level... "
+gcloud projects add-iam-policy-binding "${PROJECT_ID}" --member=serviceAccount:"${SA_EMAIL}" --role=roles/compute.instanceAdmin.v1 --format=none
+
+# Add permissions at the folder level
+echo "Adding permissions at the folder level..."
+gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=serviceAccount:"${SA_EMAIL}" --role=roles/resourcemanager.folderAdmin --format=none
+gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=serviceAccount:"${SA_EMAIL}" --role=roles/resourcemanager.projectCreator --format=none
+gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=serviceAccount:"${SA_EMAIL}" --role=roles/resourcemanager.projectDeleter --format=none
+gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=serviceAccount:"${SA_EMAIL}" --role=roles/billing.projectManager --format=none
+gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=serviceAccount:"${SA_EMAIL}" --role=roles/compute.networkAdmin --format=none
+gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=serviceAccount:"${SA_EMAIL}" --role=roles/compute.xpnAdmin --format=none
+gcloud resource-manager folders add-iam-policy-binding "${FOLDER_ID}" --member=serviceAccount:"${SA_EMAIL}" --role=roles/owner --format=none
+
+# Add permissions at the billing level
+echo "Adding permissions at the billing account level..."
+gcloud beta billing accounts get-iam-policy "${BILLING_ID}" > billing.yaml
+sa="\ \ - serviceAccount:${SA_EMAIL}"
+sed "/billing.admin/i ${sa}" billing.yaml > billing2.yaml
+gcloud beta billing accounts set-iam-policy "${BILLING_ID}" billing2.yaml --format=none
+rm billing.yaml billing2.yaml
