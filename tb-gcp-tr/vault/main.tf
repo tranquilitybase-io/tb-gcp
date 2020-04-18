a277ed17cc056935209b62ebeb5f33a95c4827d0
Lane <stln@gft.com>
2020-04-18 17:01:16 +0100

    added command for listing compute instances

--- tb-gcp-tr/vault/main.tf
+++ tb-gcp-tr/vault/main.tf
@@ -241,8 +241,8 @@ resource "null_resource" "test-ssh" {
 
   provisioner "local-exec" {
     command = <<EOF
+${local.proxy_command}"gcloud compute instances list"
 ${local.proxy_command}"gcloud container clusters get-credentials gke-sec --region=europe-west2 --project="${var.vault_cluster_project}" --internal-ip"
-
 gcloud compute ssh proxyuser@tb-kube-proxy --quiet --project="${var.shared_bastion_project}" --zone="europe-west2-a" --command="kubectl get nodes"
 
 EOF
