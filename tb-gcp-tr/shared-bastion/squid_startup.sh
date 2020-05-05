e17c6d080c040fa4da66c6a0c93cd6f5772d9244
Lane <stln@gft.com>
2020-05-05 17:40:21 +0100

    using  instead of quotes for codacy

--- tb-gcp-tr/shared-bastion/squid_startup.sh
+++ tb-gcp-tr/shared-bastion/squid_startup.sh
@@ -5,7 +5,7 @@ exec >> /var/log/bootstrap.log 2>&1
 
 echo "metadata_start"
 
-curl -sLO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+curl -sLO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
 kubectl version --client=true
