536ae8834205c326db9afaae02cdc83ed22a7c20
stuartrl07 <62605194+stuartrl07@users.noreply.github.com>
2020-05-20 10:34:53 +0100

    Update main.tf

--- tb-gcp-tr/landingZone/no-itop/main.tf
+++ tb-gcp-tr/landingZone/no-itop/main.tf
@@ -75,8 +75,7 @@ module "shared_projects" {
 
 module "apis_activation" {
   source = "../../apis-activation"
-
-  ec_project_id            = module.shared_projects.shared_ec_id
+  
   bastion_project_id       = module.shared_projects.shared_bastion_id
   host_project_id          = module.shared_projects.shared_networking_id
   itsm_project_id          = module.shared_projects.shared_itsm_id
