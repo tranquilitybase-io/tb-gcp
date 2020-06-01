c87797b93fd69861c68a90c559d4cf48babb23a7
cmac199 <21153362@student.uwa.edu.au>
2020-06-01 12:17:23 +0100

    add DNS record for eagle console

--- tb-gcp-tr/landingZone/create-private-ingress-gateway.sh
+++ tb-gcp-tr/landingZone/create-private-ingress-gateway.sh
@@ -6,4 +6,12 @@ kubectl delete svc istio-ingressgateway --namespace=istio-system
 ## record ip with Cloud DNS
 service_desc=($(kubectl describe services istio-private-ingressgateway --namespace=istio-system | grep 'LoadBalancer Ingress:'))
 endpoint_ip=${service_desc[2]}
-## todo gcloud command to add ip and domain to record set
+
+unset HTTPS_PROXY
+
+PROJECT_ID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
+TB_DISCRIMINATOR="${PROJECT_ID: -8}"
+SHARED_NETWORKING_PROJECT=shared-networking-${TB_DISCRIMINATOR}
+gcloud dns --project=${SHARED_NETWORKING_PROJECT} record-sets transaction start --zone=private-shared
+gcloud beta dns --project=${SHARED_NETWORKING_PROJECT} record-sets transaction add ${endpoint_ip} --name=eagle-console.private.landing-zone.com. --ttl=300 --type=A --zone=private-shared
+gcloud beta dns --project=${SHARED_NETWORKING_PROJECT} record-sets transaction execute --zone=private-shared
