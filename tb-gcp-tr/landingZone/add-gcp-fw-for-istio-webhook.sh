460c128f093acd83c2b7ab0cbc5876c446219e44
stuartrl07 <62605194+stuartrl07@users.noreply.github.com>
2020-06-02 15:31:29 +0100

    Update add-gcp-fw-for-istio-webhook.sh

--- tb-gcp-tr/landingZone/add-gcp-fw-for-istio-webhook.sh
+++ tb-gcp-tr/landingZone/add-gcp-fw-for-istio-webhook.sh
@@ -21,9 +21,9 @@ ${mutating_svcs}"
 rule_arr=()
 while IFS= read -r line
 do
-    svc_ns=$(echo ${line} | jq -r '.namespace')
-    svc_name=$(echo ${line} | jq -r '.name')
-    target=$(kubectl get svc -n ${svc_ns} ${svc_name} -ojson | \
+    svc_ns=$(echo "${line}" | jq -r '.namespace')
+    svc_name=$(echo "${line}" | jq -r '.name')
+    target=$(kubectl get svc -n "${svc_ns}" "${svc_name}" -ojson | \
      jq ".spec.ports[] | select( .port == 443) | .targetPort")
     if [[ $target -eq 10250 ]] || [[ $target -eq 443 ]] || [ -z "$target" ]; then
         continue
@@ -37,14 +37,14 @@ rules=${rules:1}
 # Unset so we can make gcloud commands
 unset HTTPS_PROXY
 
-source_ranges=$(gcloud container clusters describe --region=europe-west2 $CLUSTER --format="value(privateClusterConfig.masterIpv4CidrBlock)" --project=$SHARED_EC_PROJECT)
-source_tags=$(gcloud compute instances list --filter="tags.items~^gke-$CLUSTER" --limit=1 --format="value(tags.items[0])" --project=$SHARED_EC_PROJECT)
-gke_network=$(gcloud container clusters describe --region=europe-west2 $CLUSTER --format="value(network)" --project=$SHARED_EC_PROJECT)
-gcloud compute firewall-rules  create ${CLUSTER}-webhooks \
+source_ranges=$(gcloud container clusters describe --region=europe-west2 "$CLUSTER" --format="value(privateClusterConfig.masterIpv4CidrBlock)" --project="$SHARED_EC_PROJECT")
+source_tags=$(gcloud compute instances list --filter="tags.items~^gke-"$CLUSTER"" --limit=1 --format="value(tags.items[0])" --project="$SHARED_EC_PROJECT")
+gke_network=$(gcloud container clusters describe --region=europe-west2 "$CLUSTER" --format="value(network)" --project="$SHARED_EC_PROJECT")
+gcloud compute firewall-rules  create "${CLUSTER}"-webhooks \
     --action ALLOW --direction INGRESS \
-    --source-ranges $source_ranges \
-    --target-tags $source_tags \
-    --network $gke_network \
-    --rules $rules \
-    --project $SHARED_NETWORKING_PROJECT
+    --source-ranges "$source_ranges" \
+    --target-tags "$source_tags" \
+    --network "$gke_network" \
+    --rules "$rules" \
+    --project "$SHARED_NETWORKING_PROJECT"
  
