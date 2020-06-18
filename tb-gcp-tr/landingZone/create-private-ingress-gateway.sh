d84fb15493dff01caefc8a210f768847c30e0a21
afam <afam.agulonu@gft.com>
2020-06-18 21:51:09 +0100

    certs added

--- tb-gcp-tr/landingZone/create-private-ingress-gateway.sh
+++ tb-gcp-tr/landingZone/create-private-ingress-gateway.sh
@@ -4,6 +4,10 @@ kubectl apply -f /opt/tb/repo/tb-gcp-tr/landingZone/istio-pvt-ingressgateway-dep
 kubectl apply -f /opt/tb/repo/tb-gcp-tr/landingZone/istio-pvt-ingressgateway.yaml
 kubectl delete svc istio-ingressgateway --namespace=istio-system
 
+## create secret
+kubectl create -n istio-system secret tls ec-tls-credential --key=/opt/certs/eagle-console.private.landing-zone.com.key --cert=/opt/certs/eagle-console.private.landing-zone.com.crt
+
+
 ## record ip with Cloud DNS
 service_desc=($(kubectl describe services istio-private-ingressgateway --namespace=istio-system | grep 'LoadBalancer Ingress:'))
 endpoint_ip=${service_desc[2]}
@@ -15,12 +19,3 @@ SHARED_NETWORKING_PROJECT=shared-networking-${TB_DISCRIMINATOR}
 gcloud dns --project="${SHARED_NETWORKING_PROJECT}" record-sets transaction start --zone=private-shared
 gcloud beta dns --project="${SHARED_NETWORKING_PROJECT}" record-sets transaction add "${endpoint_ip}" --name=eagle-console.private.landing-zone.com. --ttl=300 --type=A --zone=private-shared
 gcloud beta dns --project="${SHARED_NETWORKING_PROJECT}" record-sets transaction execute --zone=private-shared
-
-## create certificates and keys
-#sudo mkdir /tmp/certs
-#sudo openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -subj '/O=TB Inc./CN=private.landing-zone.com' -keyout /tmp/certs/private.landing-zone.com.key -out /tmp/certs/private.landing-zone.com.crt
-#sudo openssl req -out /tmp/certs/eagle-console.private.landing-zone.com.csr -newkey rsa:2048 -nodes -keyout /tmp/certs/eagle-console.private.landing-zone.com.key -subj "/CN=eagle-console.private.landing-zone.com/O=eagle-console organization"
-#sudo openssl x509 -req -days 365 -CA /tmp/certs/private.landing-zone.com.crt -CAkey /tmp/certs/private.landing-zone.com.key -set_serial 0 -in /tmp/certs/eagle-console.private.landing-zone.com.csr -out /tmp/certs/eagle-console.private.landing-zone.com.crt
-
-## create secret
-kubectl create -n istio-system secret tls ec-tls-credential --key=/opt/certs/eagle-console.private.landing-zone.com.key --cert=/opt/certs/eagle-console.private.landing-zone.com.crt
