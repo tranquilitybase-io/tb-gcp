3b1cb8a72ad6bbba7be435a92852842185eb60df
Jezierski <e-crjk@gft.com>
2020-06-24 15:17:46 +0100

    module name fix

--- tb-gcp-tr/landingZone/no-itop/main.tf
+++ tb-gcp-tr/landingZone/no-itop/main.tf
@@ -241,7 +241,7 @@ resource "google_sourcerepo_repository_iam_binding" "terraform-code-store-admin-
   depends_on = [google_sourcerepo_repository.activator-terraform-code-store]
 }
 
-  module "logging-export-sink" {
+  module "logging_export_sink" {
     source = "../../logging-export-sink"
     
     tb_discriminator = var.tb_discriminator
