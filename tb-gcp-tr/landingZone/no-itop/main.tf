94ae5b4bdc608311e075235d93672714b4c1f2c0
Jezierski <e-crjk@gft.com>
2020-06-30 15:20:35 +0100

    reference

--- tb-gcp-tr/landingZone/no-itop/main.tf
+++ tb-gcp-tr/landingZone/no-itop/main.tf
@@ -117,7 +117,7 @@ module "bastion-security" {
 module "logging_export_sink" {
   source = "../../logging-export-sink"
   tb_discriminator              = var.tb_discriminator
-  shared_telemetry_project_name = module.folder_structure.shared_telemetry_id
+  shared_telemetry_project_name = module.shared_projects.shared_telemetry_id
   shared_services_id            = module.folder_structure.shared_services_id.name
   applications_id                 = module.folder_structure.activators_id.name
 }
