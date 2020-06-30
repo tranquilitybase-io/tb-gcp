530d80420a456894976d16a139a5391cda9e9b87
Jezierski <e-crjk@gft.com>
2020-06-30 14:34:05 +0100

    ref fixes

--- tb-gcp-tr/landingZone/no-itop/main.tf
+++ tb-gcp-tr/landingZone/no-itop/main.tf
@@ -118,8 +118,9 @@ module "logging_export_sink" {
   source = "../../logging-export-sink"
   tb_discriminator              = var.tb_discriminator
   shared_telemetry_project_name = module.folder_structure.shared_telemetry_id
-  shared_services_id            = module.folder_structure.shared_services_id
-  activators_id                 = module.folder_structure.activators_id
+  shared_services_id            = module.google_folder.shared_services_folder.name
+  activators_id                 = module.google_folder.activators_folder.name
+  
 }
 
 module "gke-ec" {
