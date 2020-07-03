b39114ce720bb2954b02987ec0c1d350b76d91ca
zain-GFT <zain.malik@gft.com>
2020-07-03 13:29:33 +0100

    HA VPN between GCP and AWS

---
+++ tb-gcp-tr/ha-vpn-aws-template/example/outputs.tf
@@ -0,0 +1,7 @@
+output vpn_interface_0_ip_address {
+  value = module.ha-vpn.vpn_interface_0_ip_address
+}
+
+output vpn_interface_1_ip_address {
+  value = module.ha-vpn.vpn_interface_1_ip_address
+}
