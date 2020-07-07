ffcbcc70540996f946431c27a126a9e3e5c38987
Jezierski <e-crjk@gft.com>
2020-07-07 17:30:43 +0100

    another sink ref

--- tb-gcp-tr/logging-folder-sink/outputs.tf
+++ tb-gcp-tr/logging-folder-sink/outputs.tf
@@ -3,5 +3,5 @@ output "shared_services_writer_identity" {
 }
 
 output "applications_writer_identity" {
-  value = google_logging_folder_sink.sink.writer_identity
+  value = google_logging_folder_sink.logging-folder-sink.writer_identity
 }
