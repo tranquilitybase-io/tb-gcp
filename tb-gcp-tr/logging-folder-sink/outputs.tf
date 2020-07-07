e15467029500e98cf5b5e3047ca3a65175e18041
Jezierski <e-crjk@gft.com>
2020-07-07 17:34:19 +0100

    typo

--- tb-gcp-tr/logging-folder-sink/outputs.tf
+++ tb-gcp-tr/logging-folder-sink/outputs.tf
@@ -1,5 +1,5 @@
 output "shared_services_writer_identity" {
-  value = google_logging_folder_sink.sink.writer_identity
+  value = google_logging_folder_sink.logging-folder-sink.writer_identity
 }
 
 output "applications_writer_identity" {
