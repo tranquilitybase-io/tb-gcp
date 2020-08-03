a25b4f53e1c0ce5f578dbedef575ca66bbde11df
Jezierski <e-crjk@gft.com>
2020-08-03 16:38:33 +0100

    removed filter for debug

--- tb-gcp-tr/logging-export/main.tf
+++ tb-gcp-tr/logging-export/main.tf
@@ -28,7 +28,8 @@ module "applications_sink" {
 
   name             = var.applications_sink_name
   folder_id        = var.applications_id
-  filter           = var.log_filter != "" ? var.log_filter : local.log_filter
+  #filter           = var.log_filter != "" ? var.log_filter : local.log_filter
+  filter = null
   include_children = var.include_children
   destination      = "storage.googleapis.com/${module.logging_buckets.names_list[0]}"
 }
@@ -38,7 +39,8 @@ module "shared_services_sink" {
 
   name             = var.shared_services_sink_name
   folder_id        = var.shared_services_id
-  filter           = var.log_filter != "" ? var.log_filter : local.log_filter
+  #filter           = var.log_filter != "" ? var.log_filter : local.log_filter
+  filter = null
   include_children = var.include_children
   destination      = "storage.googleapis.com/${module.logging_buckets.names_list[1]}"
 }
