a1cb2e9d74ed70b455073c31ed7c5215895514eb
harris-agyeman <64811624+harris-agyeman@users.noreply.github.com>
2020-08-06 12:25:49 +0100

    Update istio-services_new.sh

--- tb-gcp-tr/landingZone/istio-services_new.sh
+++ tb-gcp-tr/landingZone/istio-services_new.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 export HTTPS_PROXY="localhost:3128"
 
-istio_version=`ls -1 /opt/tb/repo/tb-gcp-tr/landingZone/no-itop/ | grep istio` 
+istio_version=$(ls -1 /opt/tb/repo/tb-gcp-tr/landingZone/no-itop/ | grep istio)
 export PATH=$PATH:/opt/tb/repo/tb-gcp-tr/landingZone/no-itop/${istio_version}/bin/
 
 
