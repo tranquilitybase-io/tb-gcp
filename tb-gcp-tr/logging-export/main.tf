a8cfa558a5871f27cae4540ee9646fc4a8527a78
Jezierski <e-crjk@gft.com>
2020-08-07 09:37:23 +0100

    removed unused code and fmt

--- tb-gcp-tr/logging-export/main.tf
+++ tb-gcp-tr/logging-export/main.tf
@@ -15,56 +15,20 @@ module "logging_buckets" {
   location        = var.location
 }
 
-# module "audit-log-writer-binding_apps" {
-#   source = "../project-iam-binding-creator"
-
-#   project = var.shared_telemetry_project_name
-#   members = module.applications_sink.unique-writer-identity
-#   role    = var.audit_iam_role
-# }
-
-# module "audit-log-writer-binding_shared" {
-#   source = "../project-iam-binding-creator"
-
-#   project = var.shared_telemetry_project_name
-#   members = module.shared_services_sink.unique-writer-identity
-#   role    = var.audit_iam_role
-# }
-
-# data "google_iam_policy" "object_creator" {
-#   binding {
-#     role = var.audit_iam_role
-#     members = [
-#       module.shared_services_sink.unique-writer-identity,
-#       module.applications_sink.unique-writer-identity
-#     ]
-#   }
-# }
-
 resource "google_storage_bucket_iam_binding" "applications_binding" {
   bucket = module.logging_buckets.names_list[0]
-  role = var.audit_iam_role
+  role   = var.audit_iam_role
   members = [
     module.applications_sink.unique-writer-identity
   ]
 }
 resource "google_storage_bucket_iam_binding" "shared_services_binding" {
   bucket = module.logging_buckets.names_list[1]
-  role = var.audit_iam_role
+  role   = var.audit_iam_role
   members = [
     module.shared_services_sink.unique-writer-identity
   ]
 }
-
-# resource "google_storage_bucket_iam_policy" "apps_bucket_object_creator_policy" {
-#   bucket      = module.logging_buckets.names_list[0]
-#   policy_data = data.google_iam_policy.object_creator.policy_data
-# }
-
-# resource "google_storage_bucket_iam_policy" "ss_bucket_object_creator_policy" {
-#   bucket      = module.logging_buckets.names_list[1]
-#   policy_data = data.google_iam_policy.object_creator.policy_data
-# }
 
 module "applications_sink" {
   source = "../logging-folder-sink"
--- tb-gcp-tr/project-iam-binding-creator/main.tf
+++
@@ -1,5 +0,0 @@
-resource "google_project_iam_binding" "bucket_audit_log_writer" {
-  project = var.project
-  members = [var.members]
-  role    = var.role
-}
