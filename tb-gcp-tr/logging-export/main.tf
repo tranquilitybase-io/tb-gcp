0faed8ff61ed10642a3788d6e912c9ab8cfae270
chrisJGFT <64899361+Waterhouse-GFT@users.noreply.github.com>
2020-10-09 10:07:43 +0100

    Fixes #462 (#485) Improve folders' logging sink filter
    Co-authored-by: Jezierski <e-crjk@gft.com>

--- tb-gcp-tr/logging-export/main.tf
+++ tb-gcp-tr/logging-export/main.tf
@@ -1,5 +1,5 @@
 locals {
-  log_filter                = "-unicorn (-Fdata_access) AND -unicorn (-Factivity) AND -unicorn (-Fsystem_event)"
+  log_filter                = "logName: ( -\"/logs/cloudaudit.googleapis.com%2Factivity\" AND -\"/logs/cloudaudit.googleapis.com%2Fdata_access\" AND -\"/logs/cloudaudit.googleapis.com%2Fsystem_event\")"
   applications_bucket_id    = "${var.applications_bucket_name}-${var.tb_discriminator}"
   shared_services_bucket_id = "${var.shared_services_bucket_name}-${var.tb_discriminator}"
 }
