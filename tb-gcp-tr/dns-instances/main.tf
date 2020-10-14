a4691b619d4abc22805c60ff104f94c40f5711c8
zain-GFT <zain.malik@gft.com>
2020-10-14 14:31:19 +0100

    Removed for_each arguments

--- tb-gcp-tr/dns-instances/main.tf
+++ tb-gcp-tr/dns-instances/main.tf
@@ -1,30 +1,26 @@
 data "google_compute_instance" "linux_instances" {
-  for_each = var.linux_instances
-  zone = var.zone
-  self_link = each.value
+  zone = "europe-west1-b"
+  self_link = element(var.linux_instances, 0)
 }
 
 data "google_compute_instance" "windows_instances" {
-  for_each = var.windows_instances
-  zone = var.zone
-  self_link = each.value
+  zone = "europe-west1-b"
+  self_link = element(var.windows_instances, 0)
 }
 
 data "google_compute_instance" "squid_proxy_instances" {
-  for_each = var.squid_proxy_instances
-  zone = var.zone
-  self_link = each.value
+  zone = "europe-west1-b"
+  self_link = element(var.squid_proxy_instances, 0)
 }
 
 resource "google_dns_record_set" "linux_instance_dns" {
-  count = length(var.linux_instances)
-  name = "tb-linux-bastion-${count.index}.${var.private_dns_domain_name}"
+  name = "tb-linux-bastion.${var.private_dns_domain_name}"
   type = "A"
   ttl  = 300
 
   managed_zone = var.private_dns_name
 
-  rrdatas = [element([for u in data.google_compute_instance.linux_instances:u.network_interface[0].network_ip], count.index)]
+  rrdatas = [data.google_compute_instance.linux_instances.network_interface[0].network_ip]
 
     depends_on = [
     data.google_compute_instance.linux_instances, var.private_dns_name, var.private_dns_domain_name
@@ -32,14 +28,13 @@ resource "google_dns_record_set" "linux_instance_dns" {
 }
 
 resource "google_dns_record_set" "windows_instance_dns" {
-  count = length(var.windows_instances)
-  name = "tb-windows-bastion-${count.index}.${var.private_dns_domain_name}"
+  name = "tb-windows-bastion.${var.private_dns_domain_name}"
   type = "A"
   ttl  = 300
 
   managed_zone = var.private_dns_name
 
-  rrdatas = [element([for u in data.google_compute_instance.windows_instances:u.network_interface[0].network_ip], count.index)]
+  rrdatas = [data.google_compute_instance.windows_instances.network_interface[0].network_ip]
 
     depends_on = [
     data.google_compute_instance.windows_instances, var.private_dns_name, var.private_dns_domain_name
@@ -47,14 +42,13 @@ resource "google_dns_record_set" "windows_instance_dns" {
 }
 
 resource "google_dns_record_set" "squid_proxy_dns" {
-  count = length(var.squid_proxy_instances)
-  name = "tb-squid-proxy-${count.index}.${var.private_dns_domain_name}"
+  name = "tb-squid-proxy.${var.private_dns_domain_name}"
   type = "A"
   ttl  = 300
 
   managed_zone = var.private_dns_name
 
-  rrdatas = [element([for u in data.google_compute_instance.squid_proxy_instances:u.network_interface[0].network_ip], count.index)]
+  rrdatas = [data.google_compute_instance.squid_proxy_instances.network_interface[0].network_ip]
 
     depends_on = [
     data.google_compute_instance.squid_proxy_instances, var.private_dns_name, var.private_dns_domain_name
