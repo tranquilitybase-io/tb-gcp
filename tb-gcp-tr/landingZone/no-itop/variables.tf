bfe09a14e11c4317b2f04234d6e5e7e5e21a76ed
zain-GFT <64900858+zain-GFT@users.noreply.github.com>
2020-11-30 10:20:04 +0000

    Update variables.tf

--- tb-gcp-tr/landingZone/no-itop/variables.tf
+++ tb-gcp-tr/landingZone/no-itop/variables.tf
@@ -244,7 +244,8 @@ variable "cluster_opt_name" {
 variable "cluster_oauth_scopes" {
   type = list(string)
   default = [
-    "https://www.googleapis.com/auth/cloud-platform"
+    "https://www.googleapis.com/auth/cloud-platform",
+    "https://www.googleapis.com/auth/projecthosting"
   ]
 }
 
