56b40d3dba5b17e45c3818458422b7c9977cd11b
Jezierski <e-crjk@gft.com>
2020-12-11 11:18:38 +0000

    removed unneeded role

--- tb-gcp-tr/shared-bastion/variables.tf
+++ tb-gcp-tr/shared-bastion/variables.tf
@@ -45,7 +45,6 @@ variable "scopes" {
 variable "main_iam_service_account_roles" {
   type = list(string)
   default = [
-    "roles/resourcemanager.folderCreator",
     "roles/resourcemanager.projectCreator",
     "roles/resourcemanager.projectDeleter",
     "roles/billing.projectManager",
