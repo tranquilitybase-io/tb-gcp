48cf23709a9f5963458014b7a18c4ca3bd3bbe3d
Jezierski <e-crjk@gft.com>
2020-12-11 09:28:48 +0000

    changed folder admin to project admin

--- tb-gcp-tr/shared-bastion/main.tf
+++ tb-gcp-tr/shared-bastion/main.tf
@@ -17,9 +17,16 @@ locals {
   service_account_name = "serviceAccount:${google_service_account.proxy-sa-res.account_id}@${var.shared_bastion_id}.iam.gserviceaccount.com"
 }
 
-resource "google_folder_iam_member" "sa-folder-admin-role" {
+# resource "google_folder_iam_member" "sa-folder-admin-role" {
+#   count      = length(var.main_iam_service_account_roles)
+#   folder     = "folders/${var.root_id}"
+#   role       = element(var.main_iam_service_account_roles, count.index)
+#   member     = local.service_account_name
+#   depends_on = [google_service_account.proxy-sa-res]
+# }
+resource "google_project_iam_member" "sa-project-admin-role" {
   count      = length(var.main_iam_service_account_roles)
-  folder     = "folders/${var.root_id}"
+  project    = var.shared_bastion_id
   role       = element(var.main_iam_service_account_roles, count.index)
   member     = local.service_account_name
   depends_on = [google_service_account.proxy-sa-res]
