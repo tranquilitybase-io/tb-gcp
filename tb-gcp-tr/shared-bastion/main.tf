c1af75a9f06d170c89cb0b5e36445ffe951d78ad
Jezierski <e-crjk@gft.com>
2020-12-14 12:27:19 +0000

    added folder iam

--- tb-gcp-tr/shared-bastion/main.tf
+++ tb-gcp-tr/shared-bastion/main.tf
@@ -17,17 +17,9 @@ locals {
   service_account_name = "serviceAccount:${google_service_account.proxy-sa-res.account_id}@${var.shared_bastion_id}.iam.gserviceaccount.com"
 }
 
-# resource "google_folder_iam_member" "sa-folder-admin-role" {
-#   count      = length(var.main_iam_service_account_roles)
-#   folder     = "folders/${var.root_id}"
-#   role       = element(var.main_iam_service_account_roles, count.index)
-#   member     = local.service_account_name
-#   depends_on = [google_service_account.proxy-sa-res]
-# }
-
-resource "google_project_iam_member" "sa-project-admin-role" {
+resource "google_folder_iam_member" "sa-folder-admin-role" {
   count      = length(var.main_iam_service_account_roles)
-  project    = var.shared_bastion_id
+  folder     = "folders/${var.root_id}"
   role       = element(var.main_iam_service_account_roles, count.index)
   member     = local.service_account_name
   depends_on = [google_service_account.proxy-sa-res]
