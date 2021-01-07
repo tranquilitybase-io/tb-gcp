0a34ca5c8c38fb99bbadf0c022d07523295618a4
zain-GFT <64900858+zain-GFT@users.noreply.github.com>
2021-01-07 10:28:50 +0000

    Update main.tf

--- tb-gcp-tr/landingZone/no-itop/main.tf
+++ tb-gcp-tr/landingZone/no-itop/main.tf
@@ -332,7 +332,7 @@ module "SharedServices_jenkinsmaster_creation" {
   cluster_context   = module.k8s-ec_context.context_name
   # Jenkins Deployment depends on the ec-service-account secret creation
   dependency_var = null_resource.kubernetes_jenkins_service_account_key_secret.id
-  depends_on = [module.dac-secret, module.gcr-configmap]
+  depends_on = [module.dac-secret, module.gcr-secret]
 }
 
 module "SharedServices_configuration_file" {
