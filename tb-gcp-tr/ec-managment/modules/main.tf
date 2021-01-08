61368b2d148b62b83a379f9edafb19785fdf7259
e-amce <adam.crow@gft.com>
2021-01-08 09:56:55 +0000

    update

--- tb-gcp-tr/ec-managment/modules/main.tf
+++ tb-gcp-tr/ec-managment/modules/main.tf
@@ -4,12 +4,12 @@ resource "random_id" "prefix" {
 
 locals {
   prefix                  = random_id.prefix.dec
-  root_folder_name        = format("TranquilityBase-", local.prefix)
-  management_project_name = format("management-", local.prefix)
-  network_name            = format("vpc-", local.prefix)
-  subnet_name             = format("subnet-", local.prefix)
-  router_name             = format("router-", local.prefix)
-  sa_name                 = format("sa-", local.prefix)
+  root_folder_name        = format("%s-%s", "TranquilityBase", local.prefix)
+  management_project_name = format("%s-%s", "management", local.prefix)
+  network_name            = format("%s-%s", "vpc", local.prefix)
+  subnet_name             = format("%s-%s", "subnet", local.prefix)
+  router_name             = format("%s-%s", "router", local.prefix)
+  sa_name                 = format("%s-%s", "sa", local.prefix)
   project_roles           = [for role in var.project_roles : format("%s=>%s", module.project.project_id, role)]
 }
 
