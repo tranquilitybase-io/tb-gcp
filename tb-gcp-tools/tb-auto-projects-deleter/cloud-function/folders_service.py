afed16e200a6f477fb7dc5b6a68ca9a1a6d21be7
Chris Jezierski <christopher.jezierski@gft.com>
2021-02-01 15:39:51 +0000

    func updates

--- tb-gcp-tools/tb-auto-projects-deleter/cloud-function/folders_service.py
+++ tb-gcp-tools/tb-auto-projects-deleter/cloud-function/folders_service.py
@@ -17,8 +17,39 @@ class FoldersService:
         resp = self.service.folders().list(parent="folders/{}".format(folder_id)).execute()
         folders = []
         if resp:
-            folders = [entry['name'].split('/')[-1] for entry in resp['folders']]
+            folders.append([entry['name'].split('/')[-1] for entry in resp['folders']])
+            for folder_ids in folders:
+                for folder in  folder_ids:
+                    folders.extend(self.get_all_child_folders(folder))
+        folders = list(dict.fromkeys(folders))
+        folders.reverse()
+        print()
         return folders
+
+    def get_all_child_folders(self,folder_id: str) -> str:
+        """
+        :param folder_id:
+        :return:
+        """
+        #get initial child folder
+        child_folders = []
+        resp = self.service.folders().list(parent="folders/{}".format(folder_id)).execute()
+        #if empty return an empty list
+        if not resp:
+            return child_folders
+        #otherwise, append the child folder to the list
+        else:
+            child_folders.append([entry['name'].split('/')[-1] for entry in resp['folders']])
+        #for each folder in the list, look for a sub folder
+        for folder in child_folders:
+            resp = self.service.folders().list(parent="folders/{}".format(folder)).execute()
+            #if there is no response, return the current list
+            if not resp:
+                return child_folders
+                #otherwise append the child folder
+            else:
+                child_folders.append([entry['name'].split('/')[-1] for entry in resp['folders']])
+        return child_folders
 
     def delete_folder(self, folder_id: str) -> str:
         """
