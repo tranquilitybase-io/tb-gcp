fbab04d3ed5f20ba8e4db18bd18f12be04a169af
Chris Jezierski <christopher.jezierski@gft.com>
2021-02-12 12:55:07 +0000

    fmt

--- tb-gcp-tools/tb-auto-projects-deleter/cloud-function/main.py
+++ tb-gcp-tools/tb-auto-projects-deleter/cloud-function/main.py
@@ -198,10 +198,6 @@ def __delete_folder(folder_id: str):
     :return:
     """
     folders_service.delete_folder(folder_id)
-<<<<<<< HEAD
-
 
 if __name__ == "__main__":
     run_delete_task()
-=======
->>>>>>> 22b4f2b8af531350e6042550fabf7a7e75242f9e
