63902005621813f57bc0afdfcb2621461b8e5656
Geoffrey Pell <Geoffrey.Pell@gft.com>
2021-02-17 10:44:21 +0000

    release management experimentation

--- build_src/deploy.sh
+++ build_src/deploy.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
 echo "I want to deploy a new release"
+
+while getopts branch_name:tag:pr: flag
+do
+    case "${flag}" in
+        branch_name) branch=${OPTARG};;
+        tag) repo_tag=${OPTARG};;
+        pr) pr_number=${OPTARG};;
+    esac
+done
+
+echo "branch: $branch";
+echo "repo_tag: $repo_tag";
+echo "pr_number: $pr_number";
