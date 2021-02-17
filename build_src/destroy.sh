b9aecb8f46b909105a6f5fab111fbde362c38568
Geoffrey Pell <Geoffrey.Pell@gft.com>
2021-02-17 11:12:16 +0000

    release management experimentation

--- build_src/destroy.sh
+++ build_src/destroy.sh
@@ -1,15 +1,36 @@
 #!/bin/sh
+set -e
 echo "I want to destroy the deployment"
 
-while getopts branch_name:tag:pr: flag
-do
-    case "${flag}" in
-        branch_name) branch=${OPTARG};;
-        tag) repo_tag=${OPTARG};;
-        pr) pr_number=${OPTARG};;
-    esac
+while (( "$#" )); do
+  case "$1" in
+    -b|--branch_name)
+      BRANCH_NAME=$2
+      shift 2
+      ;;
+    -t|--tag)
+      TAG=$2
+      shift 2
+      ;;
+    -p|--pr_number )
+      PR_NUMBER=$2
+      shift 2
+      ;;
+    --) # end argument parsing
+      shift
+      break
+      ;;
+    --*=|-*) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *) # unsupported positional arguments
+      echo "Error: Unsupported positional argument $1" >&2
+      shift
+      ;;
+  esac
 done
 
-echo "branch: $branch";
-echo "repo_tag: $repo_tag";
-echo "pr_number: $pr_number";
+echo "branch: $BRANCH_NAME";
+echo "repo_tag: $TAG";
+echo "pr_number: $PR_NUMBER";
