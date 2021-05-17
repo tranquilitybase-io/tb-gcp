37e029aa1eceade1b28681def4dbc41fdddab8ec
harris-agyeman <e-hsan@gft.com>
2021-05-17 09:59:58 +0100

    tb-new-mgmnt-plane

--- tb-gcp-tr/landingZone/tb-mgmnt-plane/4-istio-services_new.sh
+++ tb-gcp-tr/landingZone/tb-mgmnt-plane/4-istio-services_new.sh
@@ -5,8 +5,7 @@ istio_version=$(ls -1 /home/e_hsan/ | grep istio-1.9.4)
 export PATH=$PATH:/home/e_hsan/${istio_version}/bin/ 
 
 
-istioctl manifest apply --set values.kiali.enabled=true \ --set values.grafana.enabled=true
-
+kubectl apply -f  /home/e_hsan/kiali.yaml -f /home/e_hsan/grafana.yaml 
 
 kubectl apply -f /home/e_hsan/istio-kiali.yaml  -f /home/e_hsan/istio-grafana.yaml
 
